36b8c862b5456cb74bc75c23aa068f72a3bd46a8
Wei-liang Huang <will.h86@gmail.com>
2014-11-23 15:55:10 -0800

    server setup for heroku

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-/*jshint node: true*/
 'use strict';
-
 var express = require('express');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var uriUtil = require('mongodb-uri');
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } };
+var mongodbUri = process.env.MONGOLAB_URI;
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri || 'mongodb://localhost/test', options);
+
 var app = express();
+app.set('port', (process.env.PORT || 3000));
 
+app.use(bodyparser.json());
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Server started on port: %d', port);
-});
+//routes
+
+
+app.listen(app.get('port'));
